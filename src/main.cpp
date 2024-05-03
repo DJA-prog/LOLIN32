192b9f3014128b13928316876531ca6b614694dc
dinopar <almirall.dino@gmail.com>
2024-05-03 19:20:31 +0200

    GPS and SSD1306 OLED

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,33 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+#define OLED_RESET 4        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void startOLED()
+{
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  // display.setCursor(0, 5);
+  // Display static text
+  // display.println("Hello, world!");
+  // display.display();
+}
 
 #include <TinyGPSPlus.h>
 TinyGPSPlus gps;
@@ -19,6 +48,28 @@ void displayInfo()
     Serial.println("INVALID");
   }
 }
+void displayInfo1()
+{
+  Serial.print(F("Location: "));
+  if (gps.location.isValid())
+  {
+    display.clearDisplay();
+    display.setCursor(0, 2);
+    display.println("Location: ");
+    display.print("LAT: ");
+    display.println(gps.location.lat(), 6);
+    display.print("LNG: ");
+    display.println(gps.location.lng(), 6);
+    display.display();
+  }
+  else
+  {
+    display.clearDisplay();
+    display.setCursor(40, 10);
+    display.println("INVALID");
+    display.display();
+  }
+}
 
 void updateSerial()
 {
@@ -35,6 +86,8 @@ void setup()
   Serial.begin(115200);
   Serial2.begin(9600);
 
+  startOLED();
+
   pinMode(5, OUTPUT);
   analogWrite(5, 250);
 }
@@ -46,6 +99,7 @@ void loop()
     if (gps.encode(Serial2.read()))
     {
       displayInfo();
+      displayInfo1();
     }
 
   if (millis() > 5000 && gps.charsProcessed() < 10)
